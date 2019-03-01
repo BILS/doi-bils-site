716496b6d375f31b6a33119b9c96f46703aaae3c
Niclas Jareborg <niclas@jareborg.se>
2019-03-01 10:56:10 +0100

    Update query after organisation name update at Datacite to "National Bioinformatics Infrastructure Sweden"

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 
 	if( ! isset($_GET['doi'])) { // Produce the list of issued DOIs
 		// URI for all SND.BILS issued DOIs
-		$xml_uri = "https://search.datacite.org/api?q=*&fq=datacentre_facet%3A%22SND.BILS+-+Bioinformatics+Infrastructure+for+Life+Sciences%22&fl=doi,creator,title,publisher,publicationYear,datacentre&fq=is_active:true&fq=has_metadata:true&wt=xml&indent=true";
+		$xml_uri = "https://search.datacite.org/api?q=*&fq=datacentre_facet%3A%22SND.BILS+-+National+Bioinformatics+Infrastructure+Sweden%22&fl=doi,creator,title,publisher,publicationYear,datacentre&fq=is_active:true&fq=has_metadata:true&wt=xml&indent=true";
 
 		// Load XML file
 		$xml = new DOMDocument;
